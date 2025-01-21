97c34bc4ac3bc0a0a4b914d87f6bc36ae5a00ccb
TomaszHajszprych3aTP <151454968+TomaszHajszprych3aTP@users.noreply.github.com>
2025-01-21 10:29:22 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pl-PL">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
 </head>
 <body>
     <form action="" method="post">
-        <label>login</label><input type="text" name="login"><br><br>
-        <label>password</label><input type="password" name="haslo">
-        <button type="submit">Submit</button>
+        <label>Wprowadź login</label><input name="login" type="text"><br><br>
+        <label>Wprowadź hasło</label><input name="haslo" type="password"><br><br>
+        <button type="submit">Wyślij</button>
     </form>
 
 </body>
@@ -18,8 +16,8 @@
 
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $login = "admin";
-    $haslo = "test";
+    $login = "login";
+    $haslo = "password";
     if($login == $_POST["login"] and $haslo == $_POST["haslo"]){
         echo "zalogowano";
     }else{
